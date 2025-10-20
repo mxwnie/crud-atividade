c2448f6ad223c723a2d99982e34ea841238ded53
mxwnie <177383266+mxwnie@users.noreply.github.com>
2025-10-20 19:57:00 -0300

    criando o repo

---
+++ crudativ/src/main/java/com/atividade/crudativ/CrudativApplication.java
@@ -0,0 +1,13 @@
+package com.atividade.crudativ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudativApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudativApplication.class, args);
+	}
+
+}
---
+++ crudativ/src/test/java/com/atividade/crudativ/CrudativApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade.crudativ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudativApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
